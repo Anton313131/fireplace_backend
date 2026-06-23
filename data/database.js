e6fc279ea6670819f4ca2d2f7a3b4338e34985e0
Anton <ag408603@gmail.com>
2026-06-24 00:42:34 +0100

    fix: enable SSL for Render Postgres connections

--- data/database.js
+++ data/database.js
@@ -1,7 +1,12 @@
 import { Sequelize } from 'sequelize';
 import { env } from './env.js';
 
+const isRenderUrl = env.databaseUrl.includes('render.com');
+
 export const sequelize = new Sequelize(env.databaseUrl, {
   dialect: 'postgres',
   logging: false,
+  ...(isRenderUrl && {
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+  }),
 });
